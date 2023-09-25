88d7d7f8372ab958660baa07267dab826354b324
qing.zhang <qing.zhang@amlogic.com>
2023-09-25 11:03:45 +0800

    audio: [hfp_v2] dl crash with different bt devices [1/1]
    PD#SWPL-141399
    
    Problem:
    1. free buffer error when thread exit
    
    Solution:
    1. refine the buffer free control
    
    Verify:
    verify by t7c
    
    Change-Id: I35a2da7d8ff5fcd1f651e63e566005719aa9acea
    Signed-off-by: qing.zhang <qing.zhang@amlogic.com>
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- audio_hal/audio_port.c
+++ audio_hal/audio_port.c
@@ -37,6 +37,7 @@
 #include "aml_dump_debug.h"
 #include "aml_audio_spdifout.h"
 #include "tv_patch_ctrl.h"
+#include "aml_hfp.h"
 #include "aml_mmap_audio.h"
 #include "amlAudioMixer.h"
 
@@ -873,6 +874,19 @@ static ssize_t output_port_write_alsa(output_port *port, void *buffer, int bytes
         usleep(5000);
         return bytes;
     }
+
+    /* do hfp related when hfp write, for submix default frame_size 32 */
+    if (if_hfp_running_submix(port, bytes)) {
+        if (port->pcm_handle) {
+            output_port_standby(port);
+            port->pcm_handle = NULL;
+        }
+        usleep(bytes * 1000000 / 32 / 48000);
+        return bytes;
+    }
+
+    if (port->pcm_handle == NULL)
+        output_port_start(port);
 
     if (pcm_is_ready(port->pcm_handle)) {
         struct snd_pcm_status status;
