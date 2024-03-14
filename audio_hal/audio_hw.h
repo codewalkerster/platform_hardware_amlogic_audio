ca74f4e68d5a4d271465632098abd98741929951
wenbiao zhang <wenbiao.zhang@amlogic.com>
2024-03-14 19:30:45 +0800

    audio: don't write pcm until first data received [3/5]
    PD#SWPL-159811
    
    Problem:
    audio hal open pcm and write data, so tsplayer can't open pcm and play fail
    
    Solution:
    don't write pcm until first data received for tv
    
    Verify:
    T3X
    
    Change-Id: I87f36b486f86a5f0a85c26dae1783a98b3bc83b7
    Signed-off-by: wenbiao zhang <wenbiao.zhang@amlogic.com>
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- audio_hal/audio_hw.h
+++ audio_hal/audio_hw.h
@@ -554,6 +554,11 @@ struct aml_audio_device {
     /* primary streamout config format, juged by policy */
     audio_format_t primary_out_format;
     pthread_mutex_t bitstream_lock;
+
+    /* if no data write, donot open pcm device and write,
+       otherwise bootvideo can't open pcm device and play failed.*/
+    bool first_data;
+
     bool ms12_dynamic_sleep;
 };
 
