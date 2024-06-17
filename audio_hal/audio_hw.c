2b1b888ac27832774dcd334756f2ac1849537577
yongbiao.ma <yongbiao.ma@amlogic.com>
2024-06-17 11:40:59 +0000

    audio: fix cts issue. [1/1]
    PD#SWPL-173191
    
    Problem:
    Open aaudio stream without setting the submix running
    status resulted in cts fail.
    
    Solution:
    when open aaudio stream, send RUNNING msg to submix & ms12.
    
    Verify:
    Android U
    
    Change-Id: I839f95a590e1080856c8cd15b8ec0e6b8483f366
    Signed-off-by: yongbiao.ma <yongbiao.ma@amlogic.com>

--- audio_hal/audio_hw.c
+++ audio_hal/audio_hw.c
@@ -3206,6 +3206,9 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
     struct aml_stream_out *out;
     int digital_codec;
     int ret;
+    struct subMixing *sm = adev->sm;
+    struct amlAudioMixer *audio_mixer = sm ? sm->mixerData : NULL;
+    struct dolby_ms12_desc *ms12 = &(adev->ms12);
 
     out = (struct aml_stream_out *)aml_audio_calloc(1, sizeof(struct aml_stream_out));
     AM_LOGI("io %d: out:%p dev:%s(%#x) addr:%s", handle, out, audioDevType2Str(devices), devices, address);
@@ -3465,7 +3468,15 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
         const char *llp_prop = "vendor.media.llp";
         bool request_llp_mode = false;
 
-        outMmapInit(out);
+        ALOGI("when open aaudio stream, send RUNNING msg to submix & ms12");
+        aml_audiohal_sch_state_2_ms12(ms12, MS12_SCHEDULER_RUNNING);
+        aml_audiohal_sch_state_2_submix(audio_mixer, SUBMIX_SCHEDULER_RUNNING);
+        if (outMmapInit(out) != 0) {
+            AM_LOGE("outMmapInit out %p fail !", out);
+            ret = -1;
+            goto err;
+        }
+
         request_llp_mode = getprop_bool(llp_prop);
         AM_LOGI("%s %d", llp_prop, request_llp_mode);
 
@@ -3614,6 +3625,9 @@ static void adev_close_output_stream(struct audio_hw_device *dev,
 {
     struct aml_stream_out *out = (struct aml_stream_out *)stream;
     struct aml_audio_device *adev = (struct aml_audio_device *)dev;
+    struct subMixing *sm = adev->sm;
+    struct amlAudioMixer *audio_mixer = sm ? sm->mixerData : NULL;
+    struct dolby_ms12_desc *ms12 = &(adev->ms12);
 
     int ret = 0;
     int wait_cnt = 0;
@@ -3700,6 +3714,9 @@ static void adev_close_output_stream(struct audio_hw_device *dev,
             set_ms12_alsa_limit_frame(&adev->ms12, MS12_ALSA_DEFAULT_LIMIT_FRAME);  // use default limit value
         }
 #endif
+        ALOGI("when close aaudio stream, send STANDBY msg to submix & ms12");
+        aml_audiohal_sch_state_2_submix(audio_mixer, SUBMIX_SCHEDULER_STANDBY);
+        aml_audiohal_sch_state_2_ms12(ms12, MS12_SCHEDULER_STANDBY);
     }
 
     if (out->hal_format == AUDIO_FORMAT_AC4) {
