7f3415d93219ffb1bbaa20039f572548aa46703a
qiuye.gan <qiuye.gan@amlogic.com>
2024-08-29 15:49:56 +0800

    audiohal: CB2 fix avsync for ddp lib in cvbs tunne [1/1]
    PD#SWPL-131642
    
    Problem:
    DDP lib, in cvbs tunne, avsync is loss the range:
    [-40~40]
    
    Solution:
    use this property
    [vendor.media.audio.hal.nonms12.dtv.speaker]:[-40]
    
    Verify:
    s7
    
    Change-Id: Iee7a9979b3e7c85e6ee350c618802e541bf8eb1c
    Signed-off-by: qiuye.gan <qiuye.gan@amlogic.com>

--- audio_hal/audio_avsync_table_aml_ms12_v2.h
+++ audio_hal/audio_avsync_table_aml_ms12_v2.h
@@ -535,7 +535,7 @@
 #define  AVSYNC_NONMS12_DTV_HDMI_OUT_PCM_LATENCY                (-60)
 #define  AVSYNC_NONMS12_DTV_HDMI_OUT_DD_LATENCY                 (0)
 #define  AVSYNC_NONMS12_DTV_HDMI_OUT_DDP_LATENCY                (0)
-#define  AVSYNC_NONMS12_DTV_SPEAKER_LATENCY                     (0)
+#define  AVSYNC_NONMS12_DTV_SPEAKER_LATENCY                     (-40)
 
 #define  AVSYNC_NONMS12_DTV_HDMI_ARC_OUT_PCM_LATENCY_PROPERTY            "vendor.media.audio.hal.nonms12.dtv.arc.pcm"
 #define  AVSYNC_NONMS12_DTV_HDMI_ARC_OUT_DD_LATENCY_PROPERTY             "vendor.media.audio.hal.nonms12.dtv.arc.dd"
