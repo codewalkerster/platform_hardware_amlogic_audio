053ecb3811602c9f1800a8368ba897d65224cef5
Xiaoyi Zheng <xiaoyi.zheng@amlogic.com>
2025-03-13 19:23:27 +0800

    audio: ac4_de invalid when ms12 configY [1/1]
    PD#SWPL-205414
    
    Problem:
    ac4_de invalid when ms12 configY
    
    Solution:
    set ac4_de by set_ms12_decoder_parameters()
    
    Verify:
    daisy
    
    Change-Id: I6e151ab059d521498af284181acd2fc2ce0e69db
    Signed-off-by: Xiaoyi Zheng <xiaoyi.zheng@amlogic.com>

--- audio_hal/audio_hw.c
+++ audio_hal/audio_hw.c
@@ -4277,7 +4277,7 @@ static int adev_set_parameters(struct audio_hw_device *dev, const char *kvpairs)
             sprintf(parm, "%s %d", "-ac4_de", adev->ms12.ac4_de);
             pthread_mutex_lock(&adev->lock);
             if (strlen(parm) > 0)
-                aml_ms12_update_runtime_params(&(adev->ms12), parm);
+                set_ms12_decoder_parameters(adev, parm);
             pthread_mutex_unlock(&adev->lock);
             goto exit;
         }
